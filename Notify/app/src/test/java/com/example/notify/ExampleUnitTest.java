68ac945e1bcbfbe811ea4f537e88b7b9c8eeb0ce
Abonyo Mitchell Nina <157416946+ninamitchell23@users.noreply.github.com>
2025-03-31 23:01:58 +0300

    Update ExampleUnitTest.java

--- Notify/app/src/test/java/com/example/notify/ExampleUnitTest.java
+++ Notify/app/src/test/java/com/example/notify/ExampleUnitTest.java
@@ -11,7 +11,9 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testTitleSetterGetter() {
+        Notes notes = new Notes();
+        notes.setTitle("Sample Title");
+        assertEquals("Sample Title", notes.getTitle());
     }
 }
